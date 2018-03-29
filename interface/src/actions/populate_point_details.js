ed0a597f62e296984dbd43c423c84db25ea8529f
Merrin <merrin.macleod@gmail.com>
2018-03-29 13:06:04 +1100

    fix for latlng bug

--- interface/src/actions/populate_point_details.js
+++ interface/src/actions/populate_point_details.js
@@ -16,13 +16,14 @@ let _parsePointDetailsData = function(data){
   }
 }
 
-let _constructRequestString = function(source, lat, lng, query_layers, time, boundingBoxExtension = 0.05, requestFormat = "application/json", projectionFormat = "EPSG:4326") {
+let _constructRequestString = function(source, lat, lng, query_layers, time, boundingBoxExtension = 0.02, requestFormat = "application/json", projectionFormat = "EPSG:4326") {
   // Documentation for this request:
   // http://docs.geoserver.org/stable/en/user/services/wms/reference.html
-  let baseRequestParameters = "SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&X=1&Y=1&WIDTH=1&HEIGHT=1"
+  let baseRequestParameters = "SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&X=1&Y=1&WIDTH=2&HEIGHT=2"
   let parsed_time = new Date(time)
   let formatted_time = parsed_time.toISOString()
-  return(
+
+  let request_string = (
     geoserverBase
     + source
     + baseRequestParameters
@@ -31,9 +32,10 @@ let _constructRequestString = function(source, lat, lng, query_layers, time, bou
     + "&LAYERS=" + query_layers
     + "&INFO_FORMAT=" + requestFormat
     + "&EXCEPTIONS=" + requestFormat
-    + "&BBOX=" + lng + "," + lat + "," + (lng + boundingBoxExtension) + "," + (lat + boundingBoxExtension)
+    + "&BBOX=" + (lng - boundingBoxExtension) + "," + (lat - boundingBoxExtension) + "," + (lng + boundingBoxExtension) + "," + (lat + boundingBoxExtension)
     + "&TIME=" + formatted_time
   )
+  return request_string
 }
 
 export const populatePointDetailsAsync = (point, layer, time) => {
